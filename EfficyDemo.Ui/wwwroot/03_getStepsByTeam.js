4856cfac6e90a075a52da8c3cf6cfe94a90058f4
Illia Tereshchuk <illia.tereshchuk@outlook.com>
2024-11-30 14:00:41 +0100

    Added "3. Get steps by team" to UI

---
+++ EfficyDemo.Ui/wwwroot/03_getStepsByTeam.js
@@ -0,0 +1,18 @@
+﻿async function getStepsByTeamHandler() {
+    const selectTeams = document.getElementById('selectTeams3');
+    const inputTeamSteps = document.getElementById('inputTeamSteps3');
+    // Load steps on team select
+    async function onTeamSelectChange() {
+        const teamId = selectTeams.value;
+        const response = await fetch(`${apiRoot}/Teams/steps/${teamId}`);
+        const steps = await response.text();
+        inputTeamSteps.value = steps;
+    }
+    selectTeams.addEventListener('change', onTeamSelectChange);
+    // Load teams
+    const response = await fetch(`${apiRoot}/Teams/all`);
+    const teams = await response.json();
+    populateSelect(selectTeams, teams, 'id', 'name');
+}
+
+document.addEventListener('DOMContentLoaded', getStepsByTeamHandler);
