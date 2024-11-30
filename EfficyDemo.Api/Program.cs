f1bdba276c8045c9613bde4c0c91d2e1599cdb21
Illia Tereshchuk <illia.tereshchuk@outlook.com>
2024-11-30 21:27:57 +0100

    Swagger attempt #3

--- EfficyDemo.Api/Program.cs
+++ EfficyDemo.Api/Program.cs
@@ -35,7 +35,7 @@ builder.Services.AddDbContext<EfficyDbContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
